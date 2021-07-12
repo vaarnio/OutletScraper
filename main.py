6b7bc96bf43a31d627c0ad7d4e305a86583d946f
Valtteri A <vjaarn@utu.fi>
2021-07-12 19:08:16 +0300

    added tostring method and finished comparison method

--- main.py
+++ main.py
@@ -8,22 +8,25 @@ def scraper_power(session):
     r = session.get(STORE_URLS['Power'])
     json = r.json()
 
-    products = []
+    data = []
     for p in json['Products']:
-        products.append({
+        data.append({
             'product': p['SearchTitle'],
             'price': p['Price'],
             'normal_price': p['OutletProductNormalPrice'],
             'outlet_store': p['OutletStore'],
-            'outlet_reason': p['OutletReason']
+            'outlet_reason': p['OutletReason'],
+            'outlet_id' : p['OutletId']
         })
-    return products
+    return data
 
+def product_to_string(p):
+    return('Tuote: {0}\nHinta(outlet): {1}\nHinta(norm.): {2}\n{3}\n{4}\n'.format(
+            p['product'], p['price'], p['normal_price'], p['outlet_store'], p['outlet_reason']))
 
 def print_products(products):
     for p in products:
-        product_text = 'Tuote: {0}\nHinta(outlet): {1}\nHinta(norm.): {2}\n{3}\n{4}\n'.format(
-            p['product'], p['price'], p['normal_price'], p['outlet_store'], p['outlet_reason'])
+        product_text = product_to_string(p)
         print(product_text)
 
 def json_to_file(products):
@@ -37,11 +40,27 @@ def json_to_file(products):
 def test(session):
     return("testi")
 
-def compare(json_response):
-    with open('products.json', 'r') as json_file:
-        data = json_file.load(json_file)
-        for p in data['products']:
+def compare_and_get_new(products):
+    try:
+        with open('products.json', 'r') as json_file:
+            data = json.load(json_file)
+    except Exception as e:
+        print(e)
+        return([])
 
+    old_products = data['products']
+
+    old_product_ids = [p['outlet_id'] for p in old_products]
+    print(old_product_ids)
+    for old_product in old_products:
+        old_product_ids.append(old_product['outlet_id'])
+
+    new_products = []
+    for product in products:
+        if product['outlet_id'] not in old_product_ids:
+            new_products.append(product)
+
+    return(new_products)
 
 
 session = requests.Session()
@@ -50,4 +69,5 @@ products = []
 
 products += scraper_power(session)
 print_products(products)
+print(compare_and_get_new(products))
 json_to_file(products)
