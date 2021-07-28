85f0de1b19608dd278c0d989c7e27f2d83fa12af
Valtteri A <vjaarn@utu.fi>
2021-07-28 17:18:03 +0300

    Added a comment about adding new scraper methods.

--- scrapers.py
+++ scrapers.py
@@ -1,3 +1,15 @@
+""" 
+A scraper should always return a dictionary with following key:value pairs:
+    product - contains the product name
+    price - outlet price
+    normal_price - regular price
+    outlet_store - the physical store where the item is located
+    outlet_reason - for example the product could be a customer return or display piece
+    outlet_id - unique identifier scraped from the outlet store api
+
+Try to scrape all the needed information, do not generate information in the scraper method.
+"""
+
 STORE_URLS = {
     "Power": "http://www.power.fi/umbraco/api/product/getproductsbysearchrequest?f-1-BasicBrand=Apple&f-1-BasicCategories=3341&from=0&o=true&q=outlet&s=5&size=36"
 }
