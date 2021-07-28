ac1e9be132845a0c66f926450280d90db12afcca
Valtteri A <vjaarn@utu.fi>
2021-07-28 18:51:09 +0300

    refactored filtering of old products and modified main.py print statements

--- main.py
+++ main.py
@@ -26,6 +26,7 @@ def read_data_file():
             #create file if it doesn't exist
             with open('products.json', 'x') as new_file:
                 json.dump([], new_file)
+                return []
         except Exception as e:
             print(e)
             sys.exit(1)
@@ -37,35 +38,29 @@ def write_data_file(data):
     except Exception as e:
         print(e)
 
-def compare_and_get_new(products):
-    data = read_data_file()
-
-    try:
-        old_products = data['products']
-    except:
-        old_products = []
-        write_data_file(old_products)
+def filter_old_products(products):
+    old_products = read_data_file()
 
     old_product_ids = [p['outlet_id'] for p in old_products]
-    #print(old_product_ids)
-    for old_product in old_products:
-        old_product_ids.append(old_product['outlet_id'])
-
-    new_products = []
-    for product in products:
-        if product['outlet_id'] not in old_product_ids:
-            new_products.append(product)
+    new_products = [p for p in products if p['outlet_id'] not in old_product_ids]
 
     return(new_products)
 
 #scrape
+print('starting scraping')
 session = requests.Session()
 session.headers.update({'User-Agent': random.choice(USER_AGENTS)})
 products = scrapers.scrape_power(session)
+print('scraping complete')
 
-print_products(products)
-for p in compare_and_get_new(products):
+#create array containin only NEW products, THEN write ALL products to file
+new_products = filter_old_products(products)
+write_data_file(products)
+
+print('new products: \n')
+print_products(new_products)
+for p in new_products:
     notifications.notify(product_to_string(p))
 
-write_data_file(products)
+
 
