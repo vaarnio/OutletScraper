d485e25ec414fcd3945ba69ffe1b0edff2765f26
vaarnio <vaarnio@windowslive.com>
2022-05-10 13:39:52 +0300

    Moved most of the methods from main to product_helpers.py

--- main.py
+++ main.py
@@ -1,78 +1,26 @@
 import requests
-import json
 import random
-import os
-import sys
 
 import scrapers
 import notifications
+import product_helpers as p_hp
 from user_agents import USER_AGENTS
-
-PRODUCTS_JSON = 'data/products.json'
-
-def product_to_string(p):
-    return('Tuote: {0}\nHinta(outlet): {1}\nHinta(norm.): {2}\n{3}\n{4}\n'.format(
-            p['product'], p['price'], p['normal_price'], p['outlet_store'], p['outlet_reason']))
-
-def print_products(products):
-    for p in products:
-        product_text = product_to_string(p)
-        print(product_text)
-
-def read_products_file():
-    try:
-        with open(PRODUCTS_JSON, 'r') as json_file:
-            data = json.load(json_file)
-            return data
-    except FileNotFoundError:
-        try:
-            #create file if it doesn't exist
-            with open(PRODUCTS_JSON, 'x') as new_file:
-                json.dump([], new_file)
-                return []
-        except Exception as e:
-            print(e)
-            sys.exit(1)
-
-def write_products_file(data):
-    try:
-        with open(PRODUCTS_JSON, 'w') as outfile:
-            json.dump(data, outfile)
-    except Exception as e:
-        print(e)
-
-def product_old_filter(product, old_product_ids):
-    if(product['outlet_id'] in old_product_ids):
-        return False
-    else:
-        return True
-
-def filter_products(products):
-    old_products = read_products_file()
-    old_product_ids = [p['outlet_id'] for p in old_products]
-
-    # filter function passed as lambda to allow second filter function argument
-    # this way reading data_file each iteration can be avoided
-    filtfunct = lambda product: product_old_filter(product, old_product_ids)
-    new_products = list(filter(filtfunct, products))
-    
-    return(new_products)
 
 #scrape
 print('starting scraping')
 session = requests.Session()
 session.headers.update({'User-Agent': random.choice(USER_AGENTS)})
 products = scrapers.scrape_power(session, ['Apple'], ['3341'])
-print('scraping complete')
+print('scraping done')
 
 #create array containin only NEW products, THEN write ALL products to file
-new_products = filter_products(products)
-write_products_file(products)
+new_products = p_hp.filter_old_products(products)
+p_hp.write_products_file(products)
 
 print('new products: \n')
-print_products(new_products)
+p_hp.print_products(new_products)
 for p in new_products:
-    notifications.notify(product_to_string(p))
+    notifications.notify(p_hp.product_to_string(p))
 
 
 
---
+++ product_helpers.py
@@ -0,0 +1,67 @@
+import sys
+import json
+
+PRODUCTS_JSON = 'data/products.json'
+
+
+#
+# filter functions
+#
+def product_old_filter(product, old_product_ids):
+    if(product['outlet_id'] in old_product_ids):
+        return False
+    else:
+        return True
+
+
+#
+# product helpers
+#
+def product_to_string(p):
+    return('Tuote: {0}\nHinta(outlet): {1}\nHinta(norm.): {2}\n{3}\n{4}\n'.format(
+            p['product'], p['price'], p['normal_price'], p['outlet_store'], p['outlet_reason']))
+
+
+def print_products(products):
+    for p in products:
+        product_text = product_to_string(p)
+        print(product_text)
+
+
+def filter_old_products(products):
+    old_products = read_products_file()
+    old_product_ids = [p['outlet_id'] for p in old_products]
+
+    # filter function passed as lambda to allow second filter function argument
+    # this way reading data_file each iteration can be avoided
+    filtfunct = lambda product: product_old_filter(product, old_product_ids)
+    new_products = list(filter(filtfunct, products))
+    
+    return(new_products)
+
+
+#
+# product file helpers
+#
+def read_products_file():
+    try:
+        with open(PRODUCTS_JSON, 'r') as json_file:
+            data = json.load(json_file)
+            return data
+    except FileNotFoundError:
+        try:
+            #create file if it doesn't exist
+            with open(PRODUCTS_JSON, 'x') as new_file:
+                json.dump([], new_file)
+                return []
+        except Exception as e:
+            print(e)
+            sys.exit(1)
+
+
+def write_products_file(data):
+    try:
+        with open(PRODUCTS_JSON, 'w') as outfile:
+            json.dump(data, outfile)
+    except Exception as e:
+        print(e)
