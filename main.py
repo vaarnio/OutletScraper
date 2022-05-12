1539b209e1d9911e87057c07845adf77a6e91d7c
vaarnio <valtteri@vaarn.io>
2022-05-12 21:45:52 +0300

    Converted notifications.py into a class

--- main.py
+++ main.py
@@ -2,7 +2,7 @@ import random
 import requests
 
 import scrapers
-import notifications
+from notifications import NotificationService
 import product_helpers as p_hp
 from user_agents import USER_AGENTS
 
@@ -13,16 +13,19 @@ def setup_session():
 
 def main():
     session = setup_session()
-    products = scrapers.scrape_power(session, ['Apple'], ['3341'])
+    products = scrapers.scrape(session, ['Apple'], ['3341'], '0')
 
     #create array containing only NEW products, THEN write ALL products to file
     new_products = p_hp.filter_old_products(products)
     p_hp.write_products_file(products)
 
-    print('new products: \n')
-    p_hp.print_products(new_products)
-    for product in new_products:
-        notifications.notify(p_hp.product_to_string(product))
+    if(len(new_products) > 0):
+        print('new products: \n')
+        p_hp.print_products(new_products)
+
+        notif_serv = NotificationService()
+        for product in new_products:
+            notif_serv.notify(p_hp.product_to_string(product))
 
 if __name__ == "__main__":
     main()
--- notifications.py
+++ notifications.py
@@ -9,70 +9,75 @@ TOKEN = os.environ.get('outlet_bot_token')
 BOT = telegram.Bot(token=TOKEN)
 CHAT_IDS_PATHNAME = 'data/chat_ids.json'
 
-def read_chat_ids(pathname):
-    try:
-        with open(pathname, 'r') as json_file:
-            data = json.load(json_file)
-            return data
-    except FileNotFoundError:
+class NotificationService:
+
+    def __init__(self):
+        self.chat_ids = self.update_chat_ids()
+
+    @staticmethod
+    def read_chat_ids(pathname):
         try:
-            #create file if it doesn't exist
-            with open(pathname, 'x') as new_file:
-                json.dump([], new_file)
+            with open(pathname, 'r') as json_file:
+                data = json.load(json_file)
+                return data
+        except FileNotFoundError:
+            try:
+                #create file if it doesn't exist
+                with open(pathname, 'x') as new_file:
+                    json.dump([], new_file)
+            except Exception as e:
+                print(e)
+                sys.exit(1)
+    
+    @staticmethod
+    def write_chat_ids(data, pathname):
+        try:
+            with open(pathname, 'w') as outfile:
+                json.dump(data, outfile)
         except Exception as e:
             print(e)
             sys.exit(1)
-       
-
-def write_chat_ids(data, pathname):
-    try:
-        with open(pathname, 'w') as outfile:
-            json.dump(data, outfile)
-    except Exception as e:
-        print(e)
-        sys.exit(1)
 
 
-def update_chat_ids():
-    # adds new telegram chat subscribers to CHAT_IDS_PATHNAME
-    # also returns a list containing all subscribers.
+    def update_chat_ids(self):
+        # adds new telegram chat subscribers to CHAT_IDS_PATHNAME
+        # also returns a list containing all subscribers.
 
-    #get new subscribers from telegram api
-    updates = BOT.get_updates()
-    new_chat_ids = [c.message.from_user.id for c in updates]
-    new_chat_ids = list(set(new_chat_ids)) #remove duplicates by converting to set and back to list
+        #get new subscribers from telegram api
+        updates = BOT.get_updates()
+        new_chat_ids = [c.message.from_user.id for c in updates]
+        new_chat_ids = list(set(new_chat_ids)) #remove duplicates by converting to set and back to list
 
-    #get old subscribers from file
-    try:
-        chat_ids = read_chat_ids(CHAT_IDS_PATHNAME)
-        new_chat_ids = [chat for chat in new_chat_ids if chat not in chat_ids]
-        print('New telegram bot chat ids: {0}'.format(new_chat_ids))
-        chat_ids += new_chat_ids
-    except:
-        chat_ids = new_chat_ids
-
-    write_chat_ids(chat_ids, CHAT_IDS_PATHNAME)
-    return chat_ids
-
-
-def send_telegram_message(message):
-    chat_ids = update_chat_ids()
-
-    print('Sending a message to following chats: {0}'.format(chat_ids))
-    for c_id in chat_ids:
+        #get old subscribers from file
         try:
-            BOT.send_message(text=message, chat_id=c_id)
-        except telegram.error.BadRequest:
-            print('Could not send message to: {0}'.format(c_id))
-            chat_ids.remove(c_id)
-            write_chat_ids(chat_ids)
+            self.chat_ids = self.read_chat_ids(CHAT_IDS_PATHNAME)
+            new_chat_ids = [chat for chat in new_chat_ids if chat not in self.chat_ids]
+            print('New telegram bot chat ids: {0}'.format(new_chat_ids))
+            self.chat_ids += new_chat_ids
+        except:
+            self.chat_ids = new_chat_ids
+
+        self.write_chat_ids(self.chat_ids, CHAT_IDS_PATHNAME)
+        return self.chat_ids
 
 
-def notify(message):
-    # method to send message on all available notification daemons
-    send_telegram_message(message)
+    def send_telegram_message(self, message):
+        print('Sending a message to following chats: {0}'.format(self.chat_ids))
+        for c_id in self.chat_ids:
+            try:
+                BOT.send_message(text=message, chat_id=c_id, timeout=1000)
+            except telegram.error.BadRequest:
+                print('Could not send message to: {0}'.format(c_id))
+                self.chat_ids.remove(c_id)
+                self.write_chat_ids(self.chat_ids)
+
+
+    def notify(self, message):
+        # method to send message on all available notification daemons
+        self.send_telegram_message(message)
 
 
 if __name__ == "__main__":
     import sys
-    notify(sys.argv[1])
+    notif_serv = NotificationService()
+    notif_serv.notify(sys.argv[1])
