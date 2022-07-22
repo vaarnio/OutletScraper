81b878ad126d7445863fbdcefec946cc92c14066
vaarnio <valtteri@vaarn.io>
2022-07-22 22:04:05 +0300

    Inital commit for new telegram bot implementation

---
+++ bot.py
@@ -0,0 +1,28 @@
+import asyncio
+import os
+import telegram
+
+#set bot token in enrionmental variable 'outlet_bot_token' before using
+TOKEN = os.environ.get('outlet_bot_token')
+SEARCHES_JSON = 'data/searches.json'
+
+
+import logging
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CallbackContext, CommandHandler
+
+logging.basicConfig(
+    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level=logging.INFO
+)
+
+async def start(update: Update, context: CallbackContext.DEFAULT_TYPE):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a bot, please talk to me!")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TOKEN).build()
+    
+    start_handler = CommandHandler('start', start)
+    application.add_handler(start_handler)
+    
+    application.run_polling()
